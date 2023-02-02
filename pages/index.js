25e200f163f5d8449be233fdf816a4c40a0d84f5
HUAQI ZHANG <815457143ee@gmail.com>
2023-02-02 16:33:15 -0500

    add threejs package

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
-import Image from "next/image";
-import Experience from "../components/Profile/Experience";
+import { Canvas } from "@react-three/fiber";
+import Box from "../components/Box";
+import OrbitControls from "../components/OrbitControls";
+import Light from "../components/Light";
+import Floor from "../components/Floor";
+import Draggable from "../components/Draggable";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div>
-      <Image
-        priority
-        src="/images/profile.jpg"
-        className="rounded-full"
-        height={108}
-        width={108}
-        alt=""
-      />
-      <span className="text-3xl">Huaqi Zhang</span>
-      {/* <div>
-        <h1 className="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
-          hi, I'm <br className="block md:hidden" />
-          <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
-            Huaqi Zhang 👋
-          </span>
-          <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>
-        </h1>
-        <div className="text-xl font-semibold md:text-3xl">
-          developer by choice and designer for fun
-        </div>
-      </div> */}
-      <div className="h-[500px] w-[400px]">
-        <Experience />
-      </div>
+    <div className="w-screen h-screen">
+      <Canvas
+        shadows={true}
+        className="bg-blue-200"
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color={"white"} intensity={0.2} />
+        <Light position={[0, 3, 0]} />
+        <Draggable>
+          <Suspense fallback={null}>
+            <Box rotateX={3} rotateY={0.2} />
+          </Suspense>
+        </Draggable>
+        <OrbitControls />
+        <Floor position={[0, -1, 0]} />
+      </Canvas>
     </div>
   );
 }
